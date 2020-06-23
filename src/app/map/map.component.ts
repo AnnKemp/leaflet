93fa0e86d1ca67d7f0801d8613289152ca57de7e
AnnKemp <ann.kemp@scarlet.be>
2020-06-23 16:17:58 +0200

    got the card/map working, now the finishing touch

--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {AfterViewInit, Component, OnInit} from '@angular/core';
 import * as L from 'leaflet';
 
 @Component({
@@ -6,12 +6,23 @@ import * as L from 'leaflet';
   templateUrl: './map.component.html',
   styleUrls: ['./map.component.css']
 })
-export class MapComponent implements OnInit {
- private map;
+export class MapComponent implements AfterViewInit {
+ private map; // dit wordt een nieuw leaflet map object
 
   constructor() { }
 
-  ngOnInit(): void {
+  ngAfterViewInit(): void {
+    this.initMap(); // have to call the function or it doesn't work
   }
-
+private initMap():void{
+    this.map = L.map('map', {
+      center: [51.225727, 4.400853],
+      zoom: 3
+    })
+  const tiles = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',{
+    maxZoom:19,
+    attribution:'&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+  });
+    tiles.addTo(this.map);
+}
 }
